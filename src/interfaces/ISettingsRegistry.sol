2172f1db95cfbc17d2fff2a721bc510223fffa1c
echo <hujw77@gmail.com>
2021-09-09 14:11:56 +0800

    PVE

---
+++ src/interfaces/ISettingsRegistry.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.6.7;
+
+interface ISettingsRegistry {
+    function uintOf(bytes32 _propertyName) external view returns (uint256);
+    function addressOf(bytes32 _propertyName) external view returns (address);
+}
