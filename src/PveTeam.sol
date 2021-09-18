a3be04cc159ff2a0eebcb85055c3ffd010f5abfb
echo <hujw77@gmail.com>
2021-09-18 14:13:59 +0800

    pve refactor

--- src/PveTeam.sol
+++ src/PveTeam.sol
@@ -2,31 +2,33 @@ pragma solidity ^0.6.7;
 
 import "zeppelin-solidity/proxy/Initializable.sol";
 import "zeppelin-solidity/introspection/ERC165.sol";
-import "zeppelin-solidity/utils/EnumerableSet.sol";
 import "ds-auth/auth.sol";
 import "./interfaces/ISettingsRegistry.sol";
 import "./interfaces/ITokenUse.sol";
 
 contract PveTeam is Initializable, ERC165, DSAuth {
-    using EnumerableSet for EnumerableSet.UintSet;
     
-    event Join(address user, uint256 teamId, uint256 tokenId);
-    event Exit(address user, uint256 teamId, uint256 tokenId);
+    event Join(address user, uint256 slot, uint256 tokenId);
+    event Exit(address user, uint256 slot, uint256 tokenId);
 
     bytes4 internal constant InterfaceId_IActivity = 0x6086e7f8; 
 
     // 0x434f4e54524143545f544f4b454e5f5553450000000000000000000000000000
     bytes32 public constant CONTRACT_TOKEN_USE = "CONTRACT_TOKEN_USE";
-
     uint256 public constant TEAM_ID = 1; 
-
     uint256 public constant MAX_TEAM_SIZE = 4;
-    
 
     ISettingsRegistry public registry;
 
-    // owner => (teamId => set of team member)
-    mapping (address => mapping(uint256 => EnumerableSet.UintSet)) private teams;
+    struct TeamInfo {
+        address user;
+        uint256 slot;
+    }
+
+    // user => (slot => tokenId)
+    mapping (address => mapping(uint256 => uint256)) public teams;
+
+    mapping (uint256 => TeamInfo) public infos; 
 
     function initialize(address _registry) public initializer {
         owner = msg.sender;
@@ -36,61 +38,65 @@ contract PveTeam is Initializable, ERC165, DSAuth {
         _registerInterface(InterfaceId_IActivity);
     }
 
-    function joins(uint256[] calldata tokenIds) external {
+    function joins(uint256[] calldata slots, uint256[] calldata tokenIds) external {
         for(uint256 i = 0; i < tokenIds.length; i++) {
-            join(tokenIds[i]);
+            join(slots[i], tokenIds[i]);
         }
     }
 
-    function join(uint256 tokenId) public {
+    function exits(uint256[] calldata slots) external {
+        for(uint256 i = 0; i < slots.length; i++) {
+            exit(slots[i]);
+        }
+    }
+
+    function join(uint256 slot, uint256 tokenId) public {
         address tokenuse = registry.addressOf(CONTRACT_TOKEN_USE);
         ITokenUse(tokenuse).addActivity(tokenId, msg.sender, 0);
-        teams[msg.sender][TEAM_ID].add(tokenId);
-        require(teams[msg.sender][TEAM_ID].length() <= MAX_TEAM_SIZE, "Team: FULL_TEAM");
-        emit Join(msg.sender, TEAM_ID, tokenId);
-    }
-
-    function exits(uint256[] calldata tokenIds) external {
-        for(uint256 i = 0; i < tokenIds.length; i++) {
-            exit(tokenIds[i]);
-        }
+        require(slot < MAX_TEAM_SIZE, "Team: INVALID_SLOT");
+        require(teams[msg.sender][slot] == 0, "Team: OCCUPIED");
+        teams[msg.sender][slot] = tokenId;
+        infos[tokenId] = TeamInfo({
+            user: msg.sender,
+            slot: slot
+        });
+        emit Join(msg.sender, slot, tokenId);
     }
 
     function _exit(uint256 tokenId) internal {
-        teams[msg.sender][TEAM_ID].remove(tokenId);
-        emit Exit(msg.sender, TEAM_ID, tokenId);
+        TeamInfo memory info = infos[tokenId];
+        require(tokenId != 0, "Team: EMPTY");
+        delete teams[info.user][info.slot];
+        delete infos[tokenId];
+        emit Exit(info.user, info.slot, tokenId);
     }
 
     function exit(uint256 tokenId) public {
-        _exit(tokenId);
         address tokenuse = registry.addressOf(CONTRACT_TOKEN_USE);
         ITokenUse(tokenuse).removeActivity(tokenId, msg.sender);
     }
 
-    function swap(uint256 oldTokenId, uint256 newTokenId) public {
-        exit(oldTokenId);
-        join(newTokenId);
+    function swap(uint256 slot, uint256 newTokenId) public {
+        uint256 tokenId = teams[msg.sender][slot];
+        exit(tokenId);
+        join(slot, newTokenId);
     }
 
     function evict(uint256 tokenId) public {
-        _exit(tokenId);
         address tokenuse = registry.addressOf(CONTRACT_TOKEN_USE);
         ITokenUse(tokenuse).removeTokenUseAndActivity(tokenId);
     }
 
     function activityStopped(uint256 tokenId) public auth {
+        _exit(tokenId);
     }
 
-    function length(address owner) public view returns (uint256) {
-        require(owner != address(0), "Tram: ZERO_ADDRESS");
-        return teams[owner][TEAM_ID].length();
+    function at(address user, uint256 slot) public view returns (uint256) {
+        return teams[user][slot];
     }
 
-    function at(address owner, uint256 index) public view returns (uint256) {
-        return teams[owner][TEAM_ID].at(index);
+    function exist(address user, uint256 slot) public view returns (bool) {
+        return teams[user][slot] != 0;
     }
 
-    function exist(address owner, uint256 tokenId) public view returns (bool) {
-        return teams[owner][TEAM_ID].contains(tokenId);
-    }
 }
