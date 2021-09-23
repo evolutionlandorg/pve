1efb60fd87ba54521dfd7361fe2a1198cd248f73
echo <hujw77@gmail.com>
2021-09-23 15:34:53 +0800

    typo

--- src/PveTeam.sol
+++ src/PveTeam.sol
@@ -10,11 +10,11 @@ contract PveTeam is Initializable, ERC165, DSAuth {
     event Join(address user, uint256 slot, uint256 tokenId);
     event Exit(address user, uint256 slot, uint256 tokenId);
 
-    bytes4 internal constant InterfaceId_IActivity = 0x6086e7f8; 
+    bytes4 internal constant InterfaceId_IActivity = 0x6086e7f8;
     // 0x434f4e54524143545f544f4b454e5f5553450000000000000000000000000000
     bytes32 internal constant CONTRACT_TOKEN_USE = "CONTRACT_TOKEN_USE";
 
-    uint256 public constant TEAM_ID = 1; 
+    uint256 public constant TEAM_ID = 1;
     uint256 public constant MAX_TEAM_SIZE = 4;
 
     struct TeamInfo {
@@ -26,7 +26,7 @@ contract PveTeam is Initializable, ERC165, DSAuth {
     // user => (slot => tokenId)
     mapping (address => mapping(uint256 => uint256)) public teams;
     // tokenId => info
-    mapping (uint256 => TeamInfo) public infos; 
+    mapping (uint256 => TeamInfo) public infos;
 
     function initialize(address _registry) public initializer {
         owner = msg.sender;
@@ -42,9 +42,9 @@ contract PveTeam is Initializable, ERC165, DSAuth {
         }
     }
 
-    function exits(uint256[] calldata slots) external {
-        for(uint256 i = 0; i < slots.length; i++) {
-            exit(slots[i]);
+    function exits(uint256[] calldata tokenIds) external {
+        for(uint256 i = 0; i < tokenIds.length; i++) {
+            exit(tokenIds[i]);
         }
     }
 
@@ -78,11 +78,6 @@ contract PveTeam is Initializable, ERC165, DSAuth {
         uint256 tokenId = teams[msg.sender][slot];
         exit(tokenId);
         join(slot, newTokenId);
-    }
-
-    function evict(uint256 tokenId) public {
-        address tokenuse = registry.addressOf(CONTRACT_TOKEN_USE);
-        ITokenUse(tokenuse).removeTokenUseAndActivity(tokenId);
     }
 
     function activityStopped(uint256 tokenId) public auth {
